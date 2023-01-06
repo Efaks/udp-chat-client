91f9297a29a55c488f7c0eaa53cb442a45920c51
Edward Akselrud <edward.akselrud@gmail.com>
2023-01-06 09:56:11 -0500

    Add project files.

---
+++ chatClient/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace chatClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chatTextBox = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.chatLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // chatTextBox
+            // 
+            this.chatTextBox.Location = new System.Drawing.Point(12, 418);
+            this.chatTextBox.Name = "chatTextBox";
+            this.chatTextBox.Size = new System.Drawing.Size(695, 20);
+            this.chatTextBox.TabIndex = 0;
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(713, 416);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 1;
+            this.sendButton.Text = "SEND";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // chatLabel
+            // 
+            this.chatLabel.Location = new System.Drawing.Point(12, 9);
+            this.chatLabel.Name = "chatLabel";
+            this.chatLabel.Size = new System.Drawing.Size(776, 404);
+            this.chatLabel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chatLabel);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.chatTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox chatTextBox;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.Label chatLabel;
+    }
+}
+
---
+++ chatClient/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace chatClient
+{
+    public partial class Form1 : Form
+    {
+        static public UdpClient client = new UdpClient();
+        static public IPEndPoint ep = new IPEndPoint(IPAddress.Parse("10.4.37.132"), 11000);
+        public Form1()
+        {
+            InitializeComponent();
+            client.Connect(ep);
+            client.BeginReceive(new AsyncCallback(onUdpData), client);
+        }
+        public void onUdpData(IAsyncResult result)
+        {
+
+            byte[] data = client.EndReceive(result, ref ep);
+            chatLabel.Invoke((MethodInvoker)delegate { chatLabel.Text = Encoding.ASCII.GetString(data); });
+            client.BeginReceive(new AsyncCallback(onUdpData), client);
+        }
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes(chatTextBox.Text);
+            client.Send(bytes, bytes.Length);
+            chatTextBox.Text = "";
+        }
+    }
+}
